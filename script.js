34d32e2e2eb8e75f5d03ddfffb6b740d7c829259
Max Kao <mkao1@cougarmail.collin.edu>
2019-06-28 17:33:29 -0500

    added functionality for random dog breed

---
+++ script.js
@@ -0,0 +1,32 @@
+'use strict';
+
+function getDogImage(breed) {
+  fetch(`https://dog.ceo/api/breed/${breed}/images/random`)
+    .then(response => response.json())
+    .then(responseJson => 
+      displayResults(responseJson))
+    .catch(error => alert('Something went wrong. Try again later.'));
+}
+
+function displayResults(responseJson) {
+  console.log(responseJson.message);
+  $('.results-img').replaceWith(
+    `<img src="${responseJson.message}" class="results-img">`
+  )
+  $('.results').removeClass('hidden');
+
+}
+
+function watchForm() {
+  $('form').submit(event => {
+    event.preventDefault();
+    let breed = $('select').val();
+    console.log(breed);
+    getDogImage(breed);
+    })
+  }
+
+$(function() {
+  console.log('App loaded! Waiting for submit!');
+  watchForm();
+});

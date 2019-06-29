beedf24c65eedd2c7e92f461636fdf95de21ee8c
Max Kao <mkao1@cougarmail.collin.edu>
2019-06-28 21:21:00 -0500

    changed dropdown to text input with error catch

--- script.js
+++ script.js
@@ -1,26 +1,33 @@
 'use strict';
 
-function getDogImage(breed) {
+
+  function getDogImage(breed) {
   fetch(`https://dog.ceo/api/breed/${breed}/images/random`)
     .then(response => response.json())
-    .then(responseJson => 
-      displayResults(responseJson))
+    .then(responseJson => displayResults(responseJson))
     .catch(error => alert('Something went wrong. Try again later.'));
 }
 
+
 function displayResults(responseJson) {
+  console.log(responseJson);
   console.log(responseJson.message);
+  if (responseJson.status == "error") {
+    alert(responseJson.message);
+  }
+  else{
   $('.results-img').replaceWith(
     `<img src="${responseJson.message}" class="results-img">`
   )
   $('.results').removeClass('hidden');
+  }
 
 }
 
 function watchForm() {
   $('form').submit(event => {
     event.preventDefault();
-    let breed = $('select').val();
+    let breed = $('input').val();
     console.log(breed);
     getDogImage(breed);
     })
